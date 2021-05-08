39576ddb692ef952b5ca0ea3468fcc37f046a06e
Ananye <ananyesinghal@gmail.com>
2021-05-09 00:26:00 +0530

    UI created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import AddTransaction from './components/AddTransaction';
+import Balance from './components/Balance';
+import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransationList from './components/TransationList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransationList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
